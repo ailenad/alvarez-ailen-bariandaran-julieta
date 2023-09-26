e96039810add08f3da10f50f2bcf76863e9844c0
ailenad <ailen.alvarez@davinci.edu.ar>
2023-09-26 16:39:05 -0300

    Tabla profiles

--- cuidado-mascotas/database/migrations/2023_09_26_163603_create_table_profiles.php
+++ cuidado-mascotas/database/migrations/2023_09_26_163603_create_table_profiles.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('table_profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->date('birthdate');
+            $table->text('description');
+            $table->string('phone_number', 15);
             $table->timestamps();
         });
     }
